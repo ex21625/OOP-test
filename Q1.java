95aaf0511910b8212ff7e9a865073454a984c50b
“ex21625” <ex21625@qmul.ac.uk>
2024-03-15 10:28:52 +0000

    change print message

--- Q1.java
+++ Q1.java
@@ -1,7 +1,7 @@
 public class Q1{
 	public static void main(String [] args){
 		
-		System.out.println("Hello world");
+		System.out.println("Hello test");
 		System.out.println("change");
 		
 		return;
